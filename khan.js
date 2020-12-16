de173b5c7cf98597f5a2015b9b8062f365e6e3a1
FrancoFabian <jeralfabian489@gmail.com>
2020-12-16 08:12:30 -0600

    Updated

---
+++ khan.js
@@ -0,0 +1,7 @@
+var firstName="minovia";
+var lastName="miotranovia";
+var makeFunnyName = function(firstName, lastName) {
+    return "Mister " + firstName + " Mc" + lastName + "Pants";
+  };
+  makeFunnyName();
+  console.log(msg);
